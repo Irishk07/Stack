dc19d2647a1dffabbc5599997cd62f037dbc95f8
irishk07 <prygunova.irina07@mail.ru>
2025-09-25 18:44:50 +0300

    тестирование

--- main.cpp
+++ main.cpp
@@ -15,14 +15,25 @@ int main() {
         STACK_CREATE(stack, my_start_capacity);
     #endif
 
-    StackPush(&stack, 5);
-    StackPush(&stack, 9);
-    StackPush(&stack, 17);
 
-    StackPop(&stack, &delete_value);
+    // for (type_t i = 0; i < 300; ++i) {
+    //     StackPush(&stack, i);
+    // }
 
-    StackPush(&stack, 20);
+    // fprintf(stderr, "\n");
 
+    // for (type_t i = 0; i < 300; ++i) {
+    //     StackPop(&stack, &delete_value);
+    // }
+
+
+    // for (type_t i = 0; i < 10; ++i) {
+    //     StackPush(&stack, 100);
+    //     StackPush(&stack, i);
+    //     StackPop(&stack, &delete_value);
+    // }
+
+    
     StackDump(&stack, __LINE__, __FILE__, __func__, 0);
 
     StackDtor(&stack);
